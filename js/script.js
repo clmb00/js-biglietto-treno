89d7c2588b8521d5e06593f053864c2af1171326
clmb00 <paolo.colombo2000@gmail.com>
2022-10-11 16:10:20 +0200

    [ADD] style

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 const kmUser = prompt("Quanti kilometri vuole percorrere?");
 const etaUser = prompt("Quanti anni ha?");
-//const kmUser = 100;
-//const etaUser = 70;
 const scontoMinorenni = 0.2;
 const scontoOver = 0.4;
 
-let prezzoBiglietto = (kmUser * 0.21).toFixed(2);
+document.getElementById("kilometri").innerHTML = kmUser
+document.getElementById("eta").innerHTML = etaUser
+
+let prezzoBiglietto = (kmUser * 0.21);
 
 if (etaUser < 18) {
-	prezzoBiglietto -= (prezzoBiglietto * scontoMinorenni).toFixed(2);
+	prezzoBiglietto -= prezzoBiglietto * scontoMinorenni;
 } else if (etaUser > 65) {
-	prezzoBiglietto -= (prezzoBiglietto * scontoOver).toFixed(2);
+	prezzoBiglietto -= prezzoBiglietto * scontoOver;
 };
 
 document.getElementById("prezzo").innerHTML = `
-	${prezzoBiglietto} &euro;
+	${prezzoBiglietto.toFixed(2)} &euro;
 `
